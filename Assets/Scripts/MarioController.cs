248fd05bb317ded3222875b939e184203b48a49c
The Vinh Luu <luuthevinh7@gmail.com>
2016-04-28 22:49:06 +0700

    cập nhật prefap mario

---
+++ Assets/Scripts/MarioController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarioController : MonoBehaviour {
+
+    private Animator _animator;
+    private MarioMovement _marioMovement;
+
+    private float _currentSpeed = 0;
+
+    // Use this for initialization
+    void Start () {
+        _animator = this.GetComponent<Animator>();
+        _marioMovement = this.GetComponent<MarioMovement>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            if (!_animator.GetBool("isRunning"))
+                _animator.SetBool("isRunning", true);
+
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                Debug.Log(Input.GetAxis("Horizontal"));
+                _marioMovement.GotoRight();
+            }
+            else if (Input.GetAxis("Horizontal") < 0)
+            {
+                _marioMovement.GotoLeft();
+            }
+        }
+        else
+        {
+            if (_animator.GetBool("isRunning"))
+                _animator.SetBool("isRunning", false);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            _animator.SetBool("isJumping", true);
+            _marioMovement.Jump();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            _animator.SetBool("isJumping", false);
+        }
+    }
+}
---
+++ Assets/Scripts/MarioMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarioMovement : MonoBehaviour {
+
+    private Mario _mario;
+    private SpriteRenderer _spriteRenderer;
+    private Rigidbody2D _rigidbody2D;
+
+    // Use this for initialization
+    void Start () {
+        _mario = this.GetComponent<Mario>();
+        _spriteRenderer = this.GetComponent<SpriteRenderer>();
+        _rigidbody2D = this.GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void GotoLeft()
+    {
+        this.transform.Translate(Vector2.left * _mario.Velocity * Time.deltaTime);
+        _spriteRenderer.flipX = false;
+    }
+
+    public void GotoRight()
+    {
+        this.transform.Translate(Vector2.right * _mario.Velocity * Time.deltaTime);
+        _spriteRenderer.flipX = true;
+    }
+
+    public void Jump()
+    {
+        _rigidbody2D.AddForce(Vector2.up * _mario.JumpSpeed);
+    }
+}
