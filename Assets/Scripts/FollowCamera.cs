62ca55018f6cc62d6c40351a250f9e22a22d5f7a
The Vinh Luu <luuthevinh7@gmail.com>
2016-05-06 01:14:46 +0700

    thêm follow camera, cập nhật test scene

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        _camera = this.GetComponent<Camera>();
+    }
+
+    public float dampTime = 0.15f;
+    private Vector3 velocity = Vector3.zero;
+    public Transform target;
+
+    private Camera _camera;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            Vector3 point = _camera.WorldToViewportPoint(target.position);
+            Vector3 delta = target.position - _camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+            Vector3 destination = transform.position + delta;
+
+            if (destination.y < 0)
+                destination.y = 0;
+
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+            
+        }
+
+    }
+}
