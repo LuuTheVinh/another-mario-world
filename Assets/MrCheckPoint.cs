1826b31f74d8a276ae16edb5ee51e6b287e71d76
7ung <hohoangtung12a3@gmail.com>
2016-06-10 17:55:06 +0700

    commit để đi ăn. xài mario prefab của tui nhé :))

---
+++ Assets/MrCheckPoint.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MrCheckPoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        string tag = collider.gameObject.tag;
+        if (tag == "Player")
+        {
+            collider.gameObject.GetComponent<Mario>().CheckPoint = this.gameObject;
+        }
+    }
+}
